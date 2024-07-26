5a3fa0a6d5d7fb3d49e4e1bacd1b29cfc92e8fe3
Sidre Nil Denge <dengesidrenil@gmail.com>
2024-07-26 22:58:14 +0300

    edit

--- src/views/users/[id]/Posts.vue
+++ src/views/users/[id]/Posts.vue
@@ -1,7 +1,7 @@
 <template>
   <GoBackHome />
   <div class="posts-container">
-    <h1 class="text-2xl font-bold mb-5">Posts</h1>
+    <h1 class="text-2xl font-bold mb-5"></h1>
     <div v-if="posts.length === 0" class="text-gray-600">
       No posts available.
     </div>
@@ -11,6 +11,25 @@
         <p class="post-body">{{ post.body }}</p>
         <div class="post-actions">
           <button @click="openModal(post)" class="see-more-button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-crop-1-1 mr-7"
+              width="44"
+              height="44"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#4f359b"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path
+                d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"
+              />
+              <path d="M5 12l14 0" />
+              <path d="M13 18l6 -6" />
+              <path d="M13 6l6 6" />
+            </svg>
             See More
           </button>
         </div>
@@ -57,38 +76,49 @@ function closeModal() {
   selectedPost.value = null;
 }
 </script>
-
 <style scoped>
 .posts-container {
   padding: 20px;
+  padding-top: 40px;
 }
 
 .post {
-  padding-bottom: 10px;
-  margin-bottom: 10px;
+  padding-bottom: 20px;
+  margin-bottom: 20px;
 }
 
 .post-title {
-  font-size: 1.25rem;
+  font-size: 1.2rem;
   font-weight: bold;
+  margin-bottom: 10px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 }
 
 .post-body {
-  font-size: 1rem;
+  font-size: 0.9rem;
   color: #555;
+  margin-bottom: 20px;
+  max-height: 50vh;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
 }
 
 .post-actions {
   text-align: right;
-  margin-bottom: 10px;
 }
 
 .see-more-button {
   background-color: transparent;
   border: none;
-  color: blue;
+  color: darkslategray;
   cursor: pointer;
-  text-decoration: underline;
+  margin-top: 10px;
+  margin-bottom: 25px;
+  margin-right: 50px;
 }
 
 .post-divider {
